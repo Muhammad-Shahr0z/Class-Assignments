c6747fc8b282bdabf64bbf4f5a3426e83950a341
MuhammadShahroz <shahroxyt@gmail.com>
2024-12-02 01:16:30 +0500

    Updated

--- class-11/src/app/page.tsx
+++ class-11/src/app/page.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ProductCard from './components/Card'
+
+import ParentComponent from './components/Parent'
 
 const page = () => {
   return (
-    <div>
+    <div className='flex justify-center flex-col items-center h-screen bg-slate-300'>
       
-      
-      <ProductCard/>
-      
-      
+      <h1 className='text-xl text-blue-600 font-bold text-center'>Muhammad Shahroz Sunday 2 To 5 Roll Number 453242</h1>
+      <p className='font-semibold'>Class 11</p>
+      <ParentComponent/>
       </div>
   )
 }
